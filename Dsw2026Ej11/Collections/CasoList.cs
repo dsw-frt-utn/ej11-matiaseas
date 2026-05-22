262ad24b05916da221cdd9c8a2bd53a5ecaf232f
matiaseas <matias.eas.unt@gmail.com>
2026-05-22 00:33:07 -0300

    Completadas las Clases según corresponde

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -6,6 +8,33 @@
 //Incluir un método para buscar un alumno por nombre
 //Incluir un método para eliminar un alumno (debe recibir un alumno)
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
+
 public class CasoList
 {
+    private List<Alumno> _alumnos = new List<Alumno>();
+
+    public void Agregar(Alumno alumno)
+    {
+        _alumnos.Add(alumno);
+    }
+
+    public List<Alumno> GetAlumnos()
+    {
+        return _alumnos;
+    }
+
+    public Alumno? BuscarPorNombre(string nombre)
+    {
+        return _alumnos.Find(a => a.Nombre == nombre);
+    }
+
+    public void Eliminar(Alumno alumno)
+    {
+        _alumnos.Remove(alumno);
+    }
+
+    public void EliminarEnPosicion(int posicion)
+    {
+        _alumnos.RemoveAt(posicion);
+    }
 }
